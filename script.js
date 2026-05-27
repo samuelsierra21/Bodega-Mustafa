321639f21aaa43c714e4cba3ab01a8875a377a6f
samuelsierra21 <samuelsierrabarrios9@gmail.com>
2026-05-27 01:05:49 -0500

    mas configuracion

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function agregarItemAlCarrito(titulo, precio, imagenSrc){
     var nombresItemsCarrito = itemsCarrito.getElementsByClassName('carrito-item-titulo');
     for(var i=0;i < nombresItemsCarrito.length;i++){
         if(nombresItemsCarrito[i].innerText==titulo){
-            alert("El item ya se encuentra en el carrito");
+            alert("El Producto ya se encuentra en el carrito");
             return;
         }
     }
